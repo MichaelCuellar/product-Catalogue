7390443218e7ab17d33c161114ef8fc2fe40e655
Michael Cuellar <cuellar@iMac-de-Michael.local>
2021-01-13 21:38:30 -0500

    ADD: First proyect

---
+++ product_catalog_user/src/main/java/com/cuellar/product_catalog_user/ProductCatalogUserApplication.java
@@ -0,0 +1,13 @@
+package com.cuellar.product_catalog_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductCatalogUserApplication.class, args);
+    }
+
+}
---
+++ product_catalog_user/src/test/java/com/cuellar/product_catalog_user/ProductCatalogUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuellar.product_catalog_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
