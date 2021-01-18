018566001acbc72d03d0917aa30d4ef2fcaf4f1b
MichaelCuellar <mahi200@hotmail.es>
2021-01-17 21:54:39 -0500

    ADD.: Add document import.sql by category and product

---
+++ product_catalog_user/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO `role` (`id_role`, `name`) VALUES (1,'Admin');
+INSERT INTO `role` (`id_role`, `name`) VALUES (2,'Client');
+INSERT INTO `user` (`id_user`, `credentials`, `email`, `name`, `role_id_role`) VALUES (123,'123','Notificacion26@gmail.com','Test Client',2);
+INSERT INTO `user` (`id_user`, `credentials`, `email`, `name`, `role_id_role`) VALUES (1234,'1234','Notificacion26@gmail.com','Test Admin',1);
